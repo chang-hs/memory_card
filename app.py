4f2afbac3fe4745486e08253e41f61ef0ceb5379
Hansoo Chang <chang@socrate.jp>
2025-05-17 13:36:24 +0900

    Increase word length limit

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ from flask import render_template
 from models import Card, User
 from forms import CardRegisterForm, EditCardForm, LoginForm
 from models import db_session
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///memory_card.db'
-app.config['SECRET_KEY'] = 'hansoo77jp'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///memory_card.db"
+app.config["SECRET_KEY"] = "hansoo77jp"
 
 bootstrap = Bootstrap5(app)
 
@@ -20,16 +27,19 @@ locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 def set_user_password(username, password):
     new_user = User(username=username, password=generate_password_hash(password))
     db_session.add(new_user)
     db_session.commit()
+
 
 def update_user_password(username, password):
     user = User.query.filter_by(username=username).first()
@@ -37,45 +47,50 @@ def update_user_password(username, password):
         user.password = generate_password_hash(password)
     db_session.commit()
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and check_password_hash(user.password, form.password.data):
             login_user(user)
-            return redirect(url_for('list_cards', lang='all'))
+            return redirect(url_for("list_cards", lang="all"))
         else:
-            flash('Invalid username or password', 'danger')
-            return redirect(url_for('login'))
-    return render_template('login.html', form=form)
+            flash("Invalid username or password", "danger")
+            return redirect(url_for("login"))
+    return render_template("login.html", form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 @login_required
 def register():
     form = CardRegisterForm()
     if form.validate_on_submit():
-        new_card = Card(lang=form.lang.data, word=form.word.data, func=form.func.data, meaning=form.meaning.data,
-                    memo=form.memo.data)
+        new_card = Card(
+            lang=form.lang.data,
+            word=form.word.data,
+            func=form.func.data,
+            meaning=form.meaning.data,
+            memo=form.memo.data,
+        )
         db_session.add(new_card)
         db_session.commit()
-        return redirect(url_for('register'))
-    return render_template('register.html', form=form)
+        return redirect(url_for("register"))
+    return render_template("register.html", form=form)
 
-@app.route('/list/<lang>')
+
+@app.route("/list/<lang>")
 @login_required
 def list_cards(lang):
-    if lang == 'all':
+    if lang == "all":
         cards = Card.query.all()
     else:
         cards = Card.query.filter_by(lang=lang)
-    return render_template('list_card.html', title="List of cards", cards=cards)
+    return render_template("list_card.html", title="List of cards", cards=cards)
 
 
-
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
 @login_required
 def edit_card(id):
     card = db_session.query(Card).filter_by(id=id).first()
@@ -88,16 +103,18 @@ def edit_card(id):
         card.memo = form.memo.data
         card.learnt = form.learnt.data
         card.difficulty = form.difficulty.data
-        
-        db_session.commit()
-        return redirect(url_for('list_cards', lang='all'))
-    return render_template('edit_card.html', form=form)
 
-@app.route('/logout')
+        db_session.commit()
+        return redirect(url_for("list_cards", lang="all"))
+    return render_template("edit_card.html", form=form)
+
+
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=5500)
--- forms.py
+++ forms.py
@@ -2,9 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, IntegerField, DateField, BooleanField, SubmitField, PasswordField, DateTimeField
 from wtforms.validators import InputRequired, Length, Optional
 
+
 class CardRegisterForm(FlaskForm):
-    lang = SelectField('Latin', choices=['Latin', 'French', 'Korean', 'Greek', 'English'])
-    word = StringField('Word', validators=[InputRequired(),Length(max=40)])
+    lang = SelectField('Latin',
+                       choices=['Latin', 'French', 'Korean', 'Greek', 'English'])
+    word = StringField('Word', validators=[InputRequired(), Length(max=70)])
     func = StringField('Function')
     meaning = StringField('Meaning', validators=[InputRequired()])
     memo = StringField('Memo')
@@ -12,17 +14,20 @@ class CardRegisterForm(FlaskForm):
     difficult = IntegerField('Difficulty', default=0)
     submit = SubmitField('Submit')
 
+
 class CardDisplayForm(FlaskForm):
     id = IntegerField('ID')
-    word = StringField('Word', validators=[InputRequired(),Length(max=40)])
+    word = StringField('Word', validators=[InputRequired(), Length(max=70)])
     func = StringField('Function')
     meaning = StringField('Meaning', validators=[InputRequired()])
     learnt = BooleanField('Learnt')
     difficulty = IntegerField('Difficulty')
 
+
 class EditCardForm(FlaskForm):
     id = StringField('id')
-    lang = SelectField('lang', choices=['Latin', 'French', 'Korean', 'English'])
+    lang = SelectField('lang',
+                       choices=['Latin', 'French', 'Korean', 'Greek', 'English'])
     word = StringField('Word', validators=[InputRequired()])
     func = StringField('Func')
     meaning = StringField('Meaning', validators=[InputRequired()])
@@ -31,6 +36,7 @@ class EditCardForm(FlaskForm):
     difficulty = IntegerField('Difficulty')
     created_at = DateTimeField('Created')
     submit = SubmitField('Submit')
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
